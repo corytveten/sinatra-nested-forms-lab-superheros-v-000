9eb7e3421f5932c042e1323840e7d6acd5e2461f
Cory Tveten <corytveten@gmail.com>
2018-09-18 11:02:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,21 +8,22 @@ class App < Sinatra::Base
       erb :super_hero
     end
 
-    post "/teams" do
-      @team_name = params[team][name]
-      @team_motto = params[team][motto]
+    post '/teams' do
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      
+      @member1_name = params[:team][:members][0][:name]
+      @member1_power = params[:team][:members][0][:power]
+      @member1_bio = params[:team][:members][0][:bio]
 
-      @memeber1_name = params[team][members][0][name]
-      @memeber1_power = params[team][members][0][power]
-      @memeber1_bio = params[team][members][0][bio]
+      @member2_name = params[:team][:members][1][:name]
+      @member2_power = params[:team][:members][1][:power]
+      @member2_bio = params[:team][:members][1][:bio]
 
-      @memeber2_name = params[team][members][1][name]
-      @memeber2_power = params[team][members][1][power]
-      @memeber2_bio = params[team][members][1][bio]
+      @member3_name = params[:team][:members][2][:name]
+      @member3_power = params[:team][:members][2][:power]
+      @member3_bio = params[:team][:members][2][:bio]
 
-      @memeber3_name = params[team][members][2][name]
-      @memeber3_power = params[team][members][2][power]
-      @memeber3_bio = params[team][members][2][bio]
       erb :team
     end
 
